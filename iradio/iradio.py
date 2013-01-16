ae9244dc82904ef310f8932f79df71d8bf6a2fc0
linq <grazecoder@gmail.com>
2013-01-16 20:45:22 +0800

    添加参数

--- iradio/iradio.py
+++ iradio/iradio.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python  
 #coding:utf-8  
 import urllib2
-import sys
+import sys,os 
 from sgmllib import SGMLParser
 from lxml import etree as ET
 import getopt;  
@@ -14,12 +14,11 @@ def usage():
     print("Usage: [-v|-o] [--help|--output] args....");
 ver="1.8"
 configFile="~/.local/share/rhythmbox/rhythmdb.xml"
+test=False
 if "__main__" == __name__:  
-    #lsArgs = [""];  
-      
     try:  
-        opts,args = getopt.getopt(sys.argv[1:], "hv:o", ["help","version=" "output="]);  
-        for opt,arg in opts:  
+	opts,args = getopt.getopt(sys.argv[1:], "hv:o:t")  
+        for opt,arg in opts:
             if opt in ("-h", "--help"):  
                 usage();  
                 sys.exit(1);  
@@ -27,6 +26,11 @@ if "__main__" == __name__:
                 ver=arg 
             elif opt in ("-o", "--output"):  
 		configFile=arg
+            elif opt in ("-t", "--test"):  
+		test=True
+	    else:
+		usage()
+		sys.exit(1)
     except getopt.GetoptError:  
         print("getopt error!");  
         usage();  
@@ -61,15 +65,23 @@ class ListParam(SGMLParser):
 				break
 	def end_param(self):                       
 		self.asx=""
-#html µÃÂ±‡¬Î10Ω¯÷∆£¨ºÊ»›16Ω¯÷∆
+#htmlÂÆû‰ΩìÁºñÁ†Å10ËøõÂà∂ÔºåÂÖºÂÆπ16ËøõÂà∂
 def enc(unicode_data, encoding='ascii'):
 	return unicode_data.encode(encoding, 'xmlcharrefreplace')
 	#return unicode_data
 content = urllib2.urlopen('http://www.hi-fm.com/').read()
 listname = ListName()
 listname.feed(content)
+home=os.path.expanduser('~')
+configFile=configFile.replace("~",home)
+print configFile
 users = ET.Element('rhythmdb')  
-users.attrib['version'] = ver  
+users.attrib['version'] = ver 
+if os.path.exists(configFile):
+	print os.path.getsize(configFile)
+	tree=ET.parse(configFile)
+	users=tree.xpath('//rhythmdb')[0]
+	print users 
 for item in listname.urls:
 	posName=listname.urls[item].decode('gbk').encode('utf8')
 	print item+enc(posName)
@@ -87,7 +99,8 @@ for item in listname.urls:
 		radio.feed(asx)
 		asxUrl=radio.asx;
 		print asxUrl+enc(radioName)
-		if asxUrl!="":
+		findDup=tree.xpath('//rhythmdb/entry/location[text()="'+asxUrl+'"]')
+		if asxUrl!="" and findDup==[]:
 			entry = ET.SubElement(users, 'entry')
 			entry.attrib['type'] = 'iradio' 
 			title = ET.SubElement(entry, 'title')  
@@ -102,10 +115,14 @@ for item in listname.urls:
 			artist = ET.SubElement(entry, 'artist')  
 			artist.text = ""
 			album = ET.SubElement(entry, 'album')  
-			date = ET.SubElement(entry, 'date')  
+			date = ET.SubElement(entry, 'date') 
+		if test:
+			break 
+	if test:
+		break
 content=ET.tostring(users, pretty_print=True, xml_declaration=True, encoding='utf-8')
-content=content.replace("&amp;","&")
-f = open("/tmp/rhythmdb.xml", "w") 
+content=content.replace("&amp;#","&#")
+f = open(configFile, "w") 
 f.write(content)  
 f.close()
 
