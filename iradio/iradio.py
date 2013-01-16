94244c81c3bb0114c63e9bfe6ae4d817c8aceaab
linq <grazecoder@gmail.com>
2013-01-16 19:30:19 +0800

    改为lxml，解决\& 被替换的问题

--- iradio/iradio.py
+++ iradio/iradio.py
@@ -3,7 +3,7 @@
 import urllib2
 import sys
 from sgmllib import SGMLParser
-import xml.dom.minidom as Dom  
+from lxml import etree as ET
 import getopt;  
   
 default_encoding = 'utf-8'
@@ -36,7 +36,6 @@ class ListName(SGMLParser):
 		SGMLParser.__init__(self)
 		self.is_asx = ""
 		self.is_radio = ""
-		self.asx = ""
 		self.urls = {}
 		self.flag = ""
 	def handle_data(self, text):
@@ -66,45 +65,11 @@ class ListParam(SGMLParser):
 def enc(unicode_data, encoding='ascii'):
 	return unicode_data.encode(encoding, 'xmlcharrefreplace')
 	#return unicode_data
-class XMLGenerator:  
-    def __init__(self, xml_name):  
-        self.doc = Dom.Document()  
-        self.xml_name = xml_name  
-          
-    def createNode(self, node_name):  
-        return self.doc.createElement(node_name)  
-      
-    def addNode(self, node, prev_node = None):  
-        cur_node = node  
-        if prev_node is not None:  
-            prev_node.appendChild(cur_node)  
-        else:  
-            self.doc.appendChild(cur_node)  
-        return cur_node  
-  
-    def setNodeAttr(self, node, att_name, value):  
-        cur_node = node  
-        cur_node.setAttribute(att_name, value)  
-  
-    def setNodeValue(self, cur_node, value):  
-        node_data = self.doc.createTextNode(value)  
-        cur_node.appendChild(node_data)  
-  
-    def genXml(self):  
-        f = open(self.xml_name, "w")  
-        f.write(self.doc.toprettyxml(indent = "\t", newl = "\n"))  
-        f.close()  
 content = urllib2.urlopen('http://www.hi-fm.com/').read()
 listname = ListName()
 listname.feed(content)
-doc = Dom.Document()
-root_node = doc.createElement("rhythmdb")
-root_node.setAttribute("version", ver) 
-doc.appendChild(root_node)
-dbxml = XMLGenerator("/tmp/rhythmdb.xml")  
-#xml root node  
-rhythmdb = myXMLGenerator.createNode("rhythmdb")  
-myXMLGenerator.setNodeAttr(rhythmdb, "version", ver)  
+users = ET.Element('rhythmdb')  
+users.attrib['version'] = ver  
 for item in listname.urls:
 	posName=listname.urls[item].decode('gbk').encode('utf8')
 	print item+enc(posName)
@@ -116,50 +81,31 @@ for item in listname.urls:
 		radioName=listname2.urls[item2].decode('gbk').encode('utf8')
 		radioName="".join(radioName.split())
 		radioName=radioName.strip()
-		asx = urllib2.urlopen('http://www.hi-fm.com/'+muluItem).read()
+		rUrl=item2.replace("../","")
+		asx = urllib2.urlopen('http://www.hi-fm.com/'+rUrl).read()
 		radio = ListParam()
 		radio.feed(asx)
 		asxUrl=radio.asx;
 		print asxUrl+enc(radioName)
-		entry_node = doc.createElement("entry")
-		entry_title = doc.createElement("title")
-		entry_title_value=doc.createTextNode(enc(radioName))
-		entry_title.appendChild(entry_title_value)
-		entry_genre = doc.createElement("genre")
-		entry_genre_value=doc.createTextNode(enc(radioName))
-		entry_genre.appendChild(entry_genre_value)
-		entry_location = doc.createElement("location")
-		entry_location_value=doc.createTextNode(asxUrl)
-		entry_location.appendChild(entry_location_value)
-		entry_media_type = doc.createElement("media-type")
-		entry_media_type_value=doc.createTextNode("application/octet-stream")
-		entry_media_type.appendChild(entry_media_type_value)
-		
-		entry_artist = doc.createElement("artist")
-		entry_artist_value=doc.createTextNode("")
-		entry_artist.appendChild(entry_artist_value)
+		if asxUrl!="":
+			entry = ET.SubElement(users, 'entry')
+			entry.attrib['type'] = 'iradio' 
+			title = ET.SubElement(entry, 'title')  
+			title.text = enc(radioName)
+			genre = ET.SubElement(entry, 'genre')  
+			genre.text = enc(posName)
+			location = ET.SubElement(entry, 'location')  
+			location.text = asxUrl
 
-		entry_album = doc.createElement("album")
-		entry_album_value=doc.createTextNode("")
-		entry_album.appendChild(entry_album_value)
-		
-		entry_date = doc.createElement("date")
-		entry_date_value=doc.createTextNode("")
-		entry_date.appendChild(entry_date_value)
-
-		entry_node.appendChild(entry_title)
-		entry_node.appendChild(entry_genre)
-		entry_node.appendChild(entry_location)
-		entry_node.appendChild(entry_media_type)
-		entry_node.appendChild(entry_artist)
-		entry_node.appendChild(entry_album)
-		entry_node.appendChild(entry_date)
-		entry_node.setAttribute("type","iradio") 
-		root_node.appendChild(entry_node)
-		break
-	break
-f = open("", "w") 
-f.write(doc.toprettyxml(indent = "\t",newl = "\n"))  
+			media_type = ET.SubElement(entry, 'media-type')  
+			media_type.text = "application/octet-stream"
+			artist = ET.SubElement(entry, 'artist')  
+			artist.text = ""
+			album = ET.SubElement(entry, 'album')  
+			date = ET.SubElement(entry, 'date')  
+content=ET.tostring(users, pretty_print=True, xml_declaration=True, encoding='utf-8')
+content=content.replace("&amp;","&")
+f = open("/tmp/rhythmdb.xml", "w") 
+f.write(content)  
 f.close()
-
 
